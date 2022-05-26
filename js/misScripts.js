6b72aa4f00837392946dcd22bb53d4bfe9ef06ad
Lucas Medina Asevedo <lucasmedinaasevedo@gmail.com>
2022-05-26 19:03:12 -0300

    se agrego el scroll reveal

--- js/misScripts.js
+++ js/misScripts.js
@@ -38,6 +38,15 @@ for (var i = 0; i < inputs.length; i++) {
     });
 }
 
+ScrollReveal().reveal('.ppal',{delay: 500});
+ScrollReveal().reveal('.grid-cards',{delay: 500});
+ScrollReveal().reveal('.foot_cont',{delay: 500});
+ScrollReveal().reveal('.style_section',{delay: 500});
+ScrollReveal().reveal('.titulo_boton',{delay: 500});
+ScrollReveal().reveal('.ppal',{delay: 500});
+ScrollReveal().reveal('.ppal',{delay: 500});
+ScrollReveal().reveal('.ppal',{delay: 500});
+
 
 
 
@@ -117,56 +126,4 @@ function validacion(e) {
     alert("Muchas gracias por enviar el formulario");
     document.fvalida.submit();
 }
-    
 
-    
-
-// if (document.formulario.telefono.value.length ==0)
-
-//     //valido la edad. tiene que ser entero mayor que 18
-//     edad = document.fvalida.edad.value
-//     edad = validarEntero(edad)
-//     document.fvalida.edad.value = edad
-//     if (edad == "") {
-//         alert("Tiene que introducir un número entero en su edad.")
-//         document.fvalida.edad.focus()
-//         return 0;
-//     } else {
-//         if (edad < 18) {
-//             alert("Debe ser mayor de 18 años.")
-//             document.fvalida.edad.focus()
-//             return 0;
-//         }
-//     }
-
-//     //valido el interés
-//     if (document.fvalida.interes.selectedIndex == 0) {
-//         alert("Debe seleccionar un motivo de su contacto.")
-//         document.fvalida.interes.focus()
-//         return 0;
-//     }
-
-//     //el formulario se envia
-//     alert("Muchas gracias por enviar el formulario");
-//     document.fvalida.submit();
-// }
-
-// var regtel = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
-
-// 
-
-// function validarEntero(valor) {
-//     //intento convertir a entero.
-//     //si era un entero no le afecta, si no lo era lo intenta convertir
-//     valor = parseInt(valor)
-
-//     //Compruebo si es un valor numérico
-//     if (isNaN(valor)) {
-//         //entonces (no es numero) devuelvo el valor cadena vacia
-//         return ""
-//     } else {
-//         //En caso contrario (Si era un número) devuelvo el valor
-//         return valor
-//     }
-// }
-// btnEnviartodo.addEventListener('click', validacion);
